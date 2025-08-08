0158472b352fa9fe37193221d109184205d6230b
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-09 00:44:04 +1000

    CURA-19 Changed email to username for registerUser.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,13 +8,13 @@ const generateToken = (id) => {
 };
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, username, password } = req.body;
     try {
-        const userExists = await User.findOne({ email });
+        const userExists = await User.findOne({ username });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
 
-        const user = await User.create({ name, email, password });
-        res.status(201).json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+        const user = await User.create({ name, username, password });
+        res.status(201).json({ id: user.id, name: user.name, username: user.username, token: generateToken(user.id) });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
