8aa8b2f0c89f0c12e790be5151b4bc022dc0d972
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-08 23:47:29 +1000

    CURA-17 Added labels to form fields.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -21,14 +21,22 @@ const Register = () => {
     <div className="max-w-md mx-auto mt-20">
       <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded">
         <h1 className="text-2xl font-bold mb-4 text-center">Create New Account</h1>
+        
+        <label for="name">Name:</label>
         <input
+          id="name"
+          name="name"
           type="text"
           placeholder="Name"
           value={formData.name}
           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
           className="w-full mb-4 p-2 border rounded"
         />
+        
+        <label for="role">Role:</label>
         <select
+          id="role"
+          name="role"
           className="w-full mb-4 p-2 border rounded"
         >
           <option value="" disabled selected>-- Please select your role. --</option>
@@ -37,20 +45,29 @@ const Register = () => {
           <option value="Practice Manager">Practice Manager</option>
           <option value="Reception">Reception</option>
         </select>
+
+        <label for="username">Username:</label>
         <input
+          id="username"
+          name="username"
           type="text"
           placeholder="Username"
           value={formData.username}
           onChange={(e) => setFormData({ ...formData, username: e.target.value })}
           className="w-full mb-4 p-2 border rounded"
         />
+
+        <label for="password">Password:</label>
         <input
+          id="password"
+          name="password"
           type="password"
           placeholder="Password"
           value={formData.password}
           onChange={(e) => setFormData({ ...formData, password: e.target.value })}
           className="w-full mb-4 p-2 border rounded"
         />
+        
         <button type="submit" className="w-full bg-green-600 text-white p-2 rounded">
           Create
         </button>
