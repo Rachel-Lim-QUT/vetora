c6d6dfcfc4b9083ed442a9614c19fb2e45fa745c
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-10 22:23:20 +1000

    CURA-41 Added createPatient function in patientController.

---
+++ backend/controllers/patientController.js
@@ -0,0 +1,17 @@
+// Create Patient
+const createPatient = async (req, res) => {
+    const { fname, lname, dob, gender, phone, email } = req.body;
+    try {
+        const patient = await Patient.create({ patientID: req.patient.id, fname, lname, dob, gender, phone, email });
+        res.status(201).json({
+            fname: patient.fname,
+            lname: patient.lname,
+            dob: patient.dob,
+            gender: patient.gender,
+            phone: patient.phone,
+            email: patient.email,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
