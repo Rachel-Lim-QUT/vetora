d6761b3a0a2d19fd29abf903aa770d7f2485a9df
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-10 19:28:39 +1000

    CURA-39 Added placeholder content as a test.

--- frontend/src/pages/Patients.jsx
+++ frontend/src/pages/Patients.jsx
@@ -1,1 +1,9 @@
+const Patients = () => {
+    return (
+        <div className="container mx-auto p-6">
+            This is the patients page.
+        </div>
+    );
+};
+
 export default Patients;
