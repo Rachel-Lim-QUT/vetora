005e9aa66ff646669333fe794b967f611c04351a
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-10 20:51:48 +1000

    CURA-39 Added labels to form fields.

--- frontend/src/components/PatientForm.jsx
+++ frontend/src/components/PatientForm.jsx
@@ -3,25 +3,35 @@ const PatientForm = () => {
         <form className="bg-white p-6 shadow-md rounded mb-6">
             <h1 className="text-2xl font-bold mb-4">Create New Patient</h1>
 
+            <label for="fname">First Name:</label>
             <input
+                id="fname"
+                name="fname"
                 type="text"
                 placeholder="First Name"
                 className="w-full mb-4 p-2 border rounded"
             />
 
+            <label for="lname">Last Name:</label>
             <input
+                id="lname"
+                name="lname"
                 type="text"
                 placeholder="Last Name"
                 className="w-full mb-4 p-2 border rounded"
             />
 
+            <label for="dob">Date of Birth:</label>
             <input
+                id="dob"
                 name="dob"
                 type="date"
                 className="w-full mb-4 p-2 border rounded"
             />
 
+            <label for="gender">Gender:</label>
             <select
+                id="gender"
                 name="gender"
                 className="w-full mb-4 p-2 border rounded"
             >
@@ -31,14 +41,18 @@ const PatientForm = () => {
                 <option value="Other">Other</option>
             </select>
 
+            <label for="phone">Phone Number:</label>
             <input
+                id="phone"
                 name="phone"
                 type="tel"
                 placeholder="Phone Number"
                 className="w-full mb-4 p-2 border rounded"
             />
 
+            <label for="email">Email Address:</label>
             <input
+                id="email"
                 name="email"
                 type="email"
                 placeholder="Email Address"
