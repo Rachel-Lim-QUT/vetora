3d73acb99d662d539b6baf35cd62d6d452f018e2
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-11 12:24:51 +1000

    CURA-41 Fixed typo in server.js.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/patients', require ('./routes/patientRoutes'));
+app.use('/api/patients', require('./routes/patientRoutes'));
 //app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
