b8c4aa2768612c9a77e83f49480409e34f8cb864
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-12 00:04:27 +1000

    CURA-43 Removed test string from Patients page in frontend.

--- frontend/src/pages/Patients.jsx
+++ frontend/src/pages/Patients.jsx
@@ -3,7 +3,6 @@ import PatientForm from "../components/PatientForm";
 const Patients = () => {
     return (
         <div className="container mx-auto p-6">
-            This is the patients page.
             <PatientForm/>
         </div>
     );
