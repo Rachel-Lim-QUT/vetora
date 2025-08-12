c3196b629fc4028f6a54778aa55650e2d7ce1c02
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-12 22:13:13 +1000

    CURA-48 Added onClick event handler for Edit button in PatientList.

--- frontend/src/components/PatientList.jsx
+++ frontend/src/components/PatientList.jsx
@@ -1,16 +1,21 @@
-const PatientList = ({ patients }) => {
+const PatientList = ({ patients, setEditingPatient }) => {
     return (
         <div className="bg-white mb-6 p-6 rounded shadow-md">
             <h1 className="font-bold mb-4 text-2xl">Patient List</h1>
             {patients.map((patient) => (
                 <div key={patient._id} className="bg-gray-100 mb-4 p-4 rounded shadow">
+
                     <p><b>Name</b>: {patient.lname}, {patient.fname}</p>
                     <p><b>Date of Birth</b>: {patient.dob}</p>
                     <p><b>Gender</b>: {patient.gender}</p>
                     <p><b>Phone</b>: {patient.phone}</p>
                     <p><b>Email Address</b>: {patient.email}</p>
+                    
                     <div className="mt-2">
-                        <button className="bg-yellow-500 text-white px-4 py-2 rounded">
+                        <button
+                            onClick={() => setEditingPatient(patient)}
+                            className="bg-yellow-500 text-white px-4 py-2 rounded"
+                        >
                             Edit
                         </button>
                     </div>
