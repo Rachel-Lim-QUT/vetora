8199fbdc02e9d05a0faffd678a85cf5f24f03e55
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-12 20:27:51 +1000

    CURA-48 Added HTTP method for updatePatient in patientRoutes in backend.

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createPatient, getPatient } = require('../controllers/patientController');
+const { createPatient, getPatient, updatePatient } = require('../controllers/patientController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/').post(protect, createPatient);
 router.route('/').get(protect, getPatient);
+router.route('/').put(protect, updatePatient);
 
 module.exports= router;
