5ba1966ca457fdcf084cc18fd2a3eac740b3970e
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-13 11:40:36 +1000

    Changed CSS styling for PatientForm.

--- frontend/src/components/PatientForm.jsx
+++ frontend/src/components/PatientForm.jsx
@@ -44,100 +44,113 @@ const PatientForm = ({ patients, setPatients, editingPatient, setEditingPatient 
     };
 
     return (
-        <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded mb-6">
-            <h1 className="text-2xl font-bold mb-4">{editingPatient ? 'Update Patient Details' : 'Create New Patient'}</h1>
+        <div className="max-w-md">
+            <form onSubmit={handleSubmit} className="bg-white mb-6 p-6 rounded shadow-md">
+                <h1 className="font-bold text-2xl mb-4">{editingPatient ? 'Update Patient Details' : 'Create New Patient'}</h1>
 
-            <label for="fname">First Name:</label>
-            <input
-                id="fname"
-                name="fname"
-                type="text"
-                placeholder="Enter the patient's first name."
-                value={formData.fname}
-                onChange={(e) => setFormData({ ...formData, fname: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-                required
-            />
+                <label for="fname">First Name:</label>
+                <input
+                    id="fname"
+                    name="fname"
+                    type="text"
+                    placeholder="Enter the patient's first name."
+                    value={formData.fname}
+                    onChange={(e) => setFormData({ ...formData, fname: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                    required
+                />
 
-            <label for="lname">Last Name:</label>
-            <input
-                id="lname"
-                name="lname"
-                type="text"
-                placeholder="Enter the patient's last name."
-                value={formData.lname}
-                onChange={(e) => setFormData({ ...formData, lname: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-                required
-            />
+                <label for="lname">Last Name:</label>
+                <input
+                    id="lname"
+                    name="lname"
+                    type="text"
+                    placeholder="Enter the patient's last name."
+                    value={formData.lname}
+                    onChange={(e) => setFormData({ ...formData, lname: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                    required
+                />
 
-            <label for="dob">Date of Birth:</label>
-            <input
-                id="dob"
-                name="dob"
-                type="date"
-                value={formData.dob}
-                onChange={(e) => setFormData({ ...formData, dob: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-                required
-            />
+                <label for="dob">Date of Birth:</label>
+                <input
+                    id="dob"
+                    name="dob"
+                    type="date"
+                    value={formData.dob}
+                    onChange={(e) => setFormData({ ...formData, dob: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                    required
+                />
 
-            <label for="gender">Gender:</label>
-            <select
-                id="gender"
-                name="gender"
-                value={formData.gender}
-                onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-            >
-                <option value="" disabled selected>-- Select a gender. --</option>
-                <option value="Female">Female</option>
-                <option value="Male">Male</option>
-                <option value="Other">Other</option>
-            </select>
+                <label for="gender">Gender:</label>
+                <select
+                    id="gender"
+                    name="gender"
+                    value={formData.gender}
+                    onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                >
+                    <option value="" disabled selected>-- Select a gender. --</option>
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                    <option value="Other">Other</option>
+                </select>
 
-            <label for="phone">Phone Number:</label>
-            <input
-                id="phone"
-                name="phone"
-                type="tel"
-                maxLength={10}
-                placeholder="Enter the patient's phone number."
-                value={formData.phone}
-                onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-            />
+                <label for="phone">Phone Number:</label>
+                <input
+                    id="phone"
+                    name="phone"
+                    type="tel"
+                    maxLength={10}
+                    placeholder="Enter the patient's phone number."
+                    value={formData.phone}
+                    onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                />
 
-            <label for="email">Email Address:</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter the patient's email address."
-                value={formData.email}
-                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                className="w-full mb-4 p-2 border rounded"
-            />
+                <label for="email">Email Address:</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter the patient's email address."
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                    className="mb-4 p-2 w-full border rounded"
+                />
 
-            <div>
-                {editingPatient ? (
-                    <>
-                        <button type="submit" className="w-full bg-blue-600 text-white mb-4 p-2 rounded">
-                            Update
-                        </button>
-                        <button type="button" onClick={() => setEditingPatient(null)} className="w-full bg-gray-600 text-white p-2 rounded">
-                            Cancel
-                        </button>
-                    </>
-                ) : (
-                    <>
-                        <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">
-                            Create
-                        </button>
-                    </>
-                )}
-            </div>
-        </form>
+                <div>
+                    {editingPatient ? (
+                        <>
+                            <button
+                                type="submit"
+                                className="bg-blue-600 text-white mb-4 p-2 w-full rounded"
+                            >
+                                Update
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => setEditingPatient(null)}
+                                className="bg-gray-600 text-white p-2 w-full rounded"
+                            >
+                                Cancel
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <button
+                                type="submit"
+                                className="bg-blue-600 text-white p-2 w-full rounded"
+                            >
+                                Create
+                            </button>
+                        </>
+                    )}
+                </div>
+            </form>
+        </div>
+
     );
 };
 
