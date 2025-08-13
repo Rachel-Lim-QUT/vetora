6da6fe80c3e3ba1ebaa598bf20170e7f3cfdd939
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-13 11:21:22 +1000

    Removed CRUD link on Navbar in frontend.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,6 @@ const Navbar = () => {
         {user ? (
           <>
             <Link to="/patients" className="mr-4">Patients</Link>
-            <Link to="/tasks" className="mr-4">CRUD</Link>
             <Link to="/profile" className="mr-4">Account</Link>
             <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded hover:bg-red-700">
               Logout
