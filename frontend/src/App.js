7619953c300133409c154f9c05fb0cc46820ec73
jennii <jnjenniingo@gmail.com>
2025-09-14 22:12:30 +1000

    added contents into landing page and added landing page to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
@@ -9,13 +10,14 @@ import Patients from './pages/Patients';
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/tasks" element={<Tasks/>}/>
-        <Route path="/patients" element={<Patients/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/patients" element={<Patients />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,10 +1,47 @@
 const Homepage = () => {
     return (
-        <>
-            <div>
-                <h1>Welcome to the Vet Clinic Management System</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center">
+
+            {/* clinic logo */}
+            <img
+                // src={}
+                alt="Vetora logo"
+                className="w-40 h-40 mb-6"
+            />
+
+            {/* clinic name */}
+            <h1 className="text-2xl font-bold mb-8">
+                Vetora
+            </h1>
+
+            {/* buttons */}
+            <div className="flex flex-col gap-4 w-40 mx-auto">
+
+                {/* login */}
+                <button className="px-6 py-2
+                    rounded-full
+                    bg-gray-200
+                    text-gray-800
+                    text-center
+                    hover:bg-gray-300
+                    transition"
+                >
+                    Login
+                </button>
+
+                {/* register */}
+                <button className="px-6 py-2
+                    rounded-full
+                    bg-gray-200
+                    text-gray-800
+                    text-center
+                    hover:bg-gray-300
+                    transition"
+                >
+                    Register
+                </button>
             </div>
-        </>
+        </div>
     );
 };
 
