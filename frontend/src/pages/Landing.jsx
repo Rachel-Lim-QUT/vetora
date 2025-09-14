af848d4be9b7888aa9bd1d73a487e4d76359018f
jennii <jnjenniingo@gmail.com>
2025-09-14 20:58:29 +1000

    created landing page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+    return (
+        <>
+            <div>
+                <h1>Welcome to the Vet Clinic Management System</h1>
+            </div>
+        </>
+    );
+};
+
+export default Homepage;
