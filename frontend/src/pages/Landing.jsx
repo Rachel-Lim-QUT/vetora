4c368576b0b26aa06ab8780c809c998a7c12bfc7
jennii <jnjenniingo@gmail.com>
2025-09-15 11:22:14 +1000

    added back button container to login and register (unformatted)

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../images/temp-logo.gif';
 
 const Landing = () => {
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axiosInstance from '../axiosConfig';
+import backImage from '../images/temp-back.gif';
+import '../App.css';
 
 const Register = () => {
   const [formData, setFormData] = useState({ name: '', role: '', username: '', password: '' });
@@ -18,65 +20,89 @@ const Register = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-20">
-      <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded">
-        <h1 className="text-2xl font-bold mb-4 text-center">Create New Account</h1>
-        
-        <label for="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Enter your full name."
-          value={formData.name}
-          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          className="w-full mb-4 p-2 border rounded"
-          required
-        />
-        
-        <label for="role">Role:</label>
-        <select
-          id="role"
-          name="role"
-          onChange={(e) => setFormData({ ...formData, role: e.target.value })}
-          className="w-full mb-4 p-2 border rounded"
-          required
+    <div>
+      <div className="max-w-md mx-auto">
+        <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded">
+          <h1 className="text-2xl font-bold mb-4 text-center">Create New Account</h1>
+
+          <label for="name">Name:</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter your full name."
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            className="w-full mb-4 p-2 border rounded"
+            required
+          />
+
+          <label for="role">Role:</label>
+          <select
+            id="role"
+            name="role"
+            onChange={(e) => setFormData({ ...formData, role: e.target.value })}
+            className="w-full mb-4 p-2 border rounded"
+            required
+          >
+            <option value="" disabled selected>-- Please select your role. --</option>
+            <option value="Doctor">Doctor</option>
+            <option value="Nurse">Nurse</option>
+            <option value="Practice Manager">Practice Manager</option>
+            <option value="Reception">Reception</option>
+          </select>
+
+          <label for="username">Username:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Enter your username."
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+            className="w-full mb-4 p-2 border rounded"
+            required
+          />
+
+          <label for="password">Password:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password."
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            className="w-full mb-4 p-2 border rounded"
+            required
+          />
+
+          <button type="submit" className="w-full bg-green-600 text-white p-2 rounded">
+            Create
+          </button>
+        </form>
+      </div>
+
+      {/* back container */}
+      <div className="back-container">
+        {/* back button */}
+        <Link to="/" type="submit" className="px-6 py-2
+                    rounded-full
+                    bg-gray-200
+                    text-gray-800
+                    text-center
+                    hover:bg-gray-300"
         >
-          <option value="" disabled selected>-- Please select your role. --</option>
-          <option value="Doctor">Doctor</option>
-          <option value="Nurse">Nurse</option>
-          <option value="Practice Manager">Practice Manager</option>
-          <option value="Reception">Reception</option>
-        </select>
+          Back
+        </Link>
 
-        <label for="username">Username:</label>
-        <input
-          id="username"
-          name="username"
-          type="text"
-          placeholder="Enter your username."
-          value={formData.username}
-          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-          className="w-full mb-4 p-2 border rounded"
-          required
+        {/* back image */}
+        <img
+          src={backImage}
+          alt="Back image"
+          className="back-image"
         />
+      </div>
 
-        <label for="password">Password:</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Enter your password."
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-          className="w-full mb-4 p-2 border rounded"
-          required
-        />
-
-        <button type="submit" className="w-full bg-green-600 text-white p-2 rounded">
-          Create
-        </button>
-      </form>
     </div>
   );
 };
