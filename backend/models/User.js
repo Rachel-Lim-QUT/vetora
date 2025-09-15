0038bd07461a355a6c0b5aa67ebe50ec79945cba
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-16 00:49:14 +1000

    Removed trim on fname and lname as well as unnecessary next() function in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,19 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-
 const userSchema = new mongoose.Schema({
-  fname:    { type: String, required: true },
-  lname:    { type: String, required: true },
-  clinic:   { type: String, required: true },
-  role:     { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    fname:      { type: String, required: true },
+    lname:      { type: String, required: true },
+    clinic:     { type: String, required: true },
+    role:       { type: String, required: true },
+    username:   { type: String, required: true, unique: true },
+    password:   { type: String, required: true },
 });
 
 userSchema.pre('save', async function (next) {
-  try {
-    
-    this.name = `${this.fname || ''} ${this.lname || ''}`.trim();
-
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
 });
 
 module.exports = mongoose.model('User', userSchema);
