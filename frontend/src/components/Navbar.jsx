825c8d99bcb60233c5b49e6fc8bb86cf31f225e2
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-16 10:30:49 +1000

    Renamed Account to Profile on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         {user ? (
           <>
             <Link to="/patients" className="mr-4">Patients</Link>
-            <Link to="/profile" className="mr-4">Account</Link>
+            <Link to="/profile" className="mr-4">Profile</Link>
             <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded hover:bg-red-700">
               Logout
             </button>
