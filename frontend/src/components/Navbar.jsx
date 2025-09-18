584ee0598b7b3685d6b851414b5e00f4d7112e7e
jennii <jnjenniingo@gmail.com>
2025-09-18 13:20:58 +1000

    added logo to navbar, and updated logo and vetora nagivation to homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import logo from '../images/temp-logo.gif';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -12,7 +13,17 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <Link to="/patients" className="text-2xl font-bold">Vetora</Link>
+      <div className="flex item-container">
+        <Link to="/homepage">
+          <img
+            src={logo}
+            alt="logo"
+            className="h-8 w-8 mr-2" />
+        </Link>
+        <Link to="/homepage" className="text-2xl font-bold">
+          Vetora
+        </Link>
+      </div>
       <div>
         <Link to="" className="mr-4">Appointments</Link>
         <Link to="/patients" className="mr-4">Patients</Link>
