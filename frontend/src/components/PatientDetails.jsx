8b7d119c107130a50181a70938e0ffebc198c94f
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-25 20:42:20 +1000

    Removed comments that are no longer needed

--- frontend/src/components/PatientDetails.jsx
+++ frontend/src/components/PatientDetails.jsx
@@ -60,7 +60,6 @@ const PatientDetails = ({ patients, setPatients }) => {
             });
             alert('Success! Patient deleted.')
             navigate('/patients');
-            // setPatients(patients.filter((patient) => patient._id !== patientID));
         } catch (error) {
             alert('Error: Failed to delete patient.')
         } finally {
@@ -235,7 +234,6 @@ const PatientDetails = ({ patients, setPatients }) => {
                             <>
                             <button onClick={startEdit} className="pill-button bg-yellow-500 text-white px-4 py-2 rounded">Edit</button>
                             <button
-                                // onClick={() => handleDelete(patients._id)}
                                 onClick={() => setShowConfirm(true)}
                                 className="pill-button bg-red-500 text-white ml-2 px-4 py-2 rounded"
                             >
