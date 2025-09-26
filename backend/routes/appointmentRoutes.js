bf123257774172de25c6998f84140f918114b8a2
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-26 23:07:36 +1000

    Added getAppointment to appointmentRoutes in backend

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createAppointment } = require('../controllers/appointmentController');
+const { createAppointment, getAppointment } = require('../controllers/appointmentController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/', protect, createAppointment);
+router.get('/:id', protect, getAppointment);
 
 module.exports = router;
