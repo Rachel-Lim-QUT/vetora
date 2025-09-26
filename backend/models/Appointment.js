18ca0af46413bc7187eb71ddc803dff4a6fd8553
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-26 20:07:59 +1000

    Changed Appointment model in backend

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,13 +1,11 @@
-import { Schema, model } from 'mongoose';
+const mongoose = require('mongoose');
 
-const appointmentSchema = new Schema({
-    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    patient: { type: Schema.Types.ObjectId, ref: 'Patient', required: true },
-    status: { type: String, required: true }, // Rachel's note: For example, Upcoming, In Progress, Complete, etc.
-    date: { type: String, required: true },
-    time: { type: String, required: true },
+const appointmentSchema = new mongoose.Schema({
+    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    patient: { type: String, required: true },
     type: { type: String, required: true },
-    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+    completed: { type: Boolean, default: false },
 });
 
-export default model('Appointment', appointmentSchema);
+module.exports = mongoose.model('Appointment', appointmentSchema);
