baff69377695e2c8f4a6b28423e1091223ea29e6
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-26 13:23:23 +1000

    Added Appointments route to App.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Homepage from './pages/Homepage';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Appointments from './pages/Appointments';
 import Patients from './pages/Patients';
 import PatientProfile from './pages/PatientProfile';
 
 function App() {
-  return (
-    <Router>
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/patients" element={<Patients />} />
-        <Route path="/patientprofile/:id" element={<PatientProfile />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/homepage" element={<Homepage />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/patients" element={<Patients />} />
+                <Route path="/patientprofile/:id" element={<PatientProfile />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
