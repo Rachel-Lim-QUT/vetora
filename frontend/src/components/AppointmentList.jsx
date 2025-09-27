dad48c807202337c95194aefd712ab1019260b40
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-09-27 12:52:12 +1000

    Removed status from AppointmentList component in frontend

--- frontend/src/components/AppointmentList.jsx
+++ frontend/src/components/AppointmentList.jsx
@@ -7,7 +7,6 @@ const AppointmentList = ({ appointments }) => {
                     <p><b>Patient</b>: {appointment.patient}</p>
                     <p><b>Type</b>: {appointment.type}</p>
                     <p><b>Date</b>: {Date(appointment.date)}</p>
-                    <p><b>Status</b>: {appointment.completed}</p>
 
                     <div className="mt-2">
                         <button className="pill-button bg-yellow-500 hover:bg-yellow-600 text-white mr-2 px-4 py-2 shadow">
